69669e83399cd52c1337f592877a5e881530e720
Mark Hill <futonfutonda@gmail.com>
2016-01-12 12:27:41 +0000

    new test/refactor

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,4 +6,11 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
   expect(fizzbuzz(5)).to eq 'buzz'
   end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "2" when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
+  end
+
 end
